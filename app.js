775eba8ad73f3bc444231775c341f5bcd0cd6f15
Brandon Yang <brandon.yang1210@gmail.com>
2025-01-06 23:50:43 -0500

    i changed the bar at the top where it displays the list and changed it to something else where its more interactive. I change the styling for it as well and changed some of the styling for the others as well.

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 //Function to display the other links if you click on the hamburger or not
 function myFunction() {
-    const links = document.getElementById("myLinks");
-    if (links.style.display === "block") {
-      links.style.display = "none";
-    } else {
-      links.style.display = "block";
-    }
+  const links = document.getElementById("myLinks");
+  if (links.style.display === "flex") {
+    links.style.display = "none";
+  } else {
+    links.style.display = "flex";
   }
+}
